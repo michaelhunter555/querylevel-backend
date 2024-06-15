2f6f4a39667dcec314a77c3c871d09dd916c218f
michaelhunter555 <dumbfreshdope@gmail.com>
2024-06-15 14:29:00 +0700

    remove preFlight false on cors

--- app.ts
+++ app.ts
@@ -19,15 +19,13 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    "https://519f5f06cbd0.ngrok.app", // Replace with your frontend origin
-    "https://89477cd74c6f.ngrok.app",
+    // "https://519f5f06cbd0.ngrok.app", // Replace with your frontend origin
+    // "https://89477cd74c6f.ngrok.app",
     "https://query-level-bbd02bb2f6c4.herokuapp.com",
     "https://querylevel.com", // Replace with your backend origin https://querylevel.com/
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
